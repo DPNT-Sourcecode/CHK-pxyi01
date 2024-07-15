30987980fd8494b64d6e032aed4fb116c5665041
sbx_user1051 <sbx_user1051@169.254.52.149>
2024-07-15 16:21:15 +0000

    Mon Jul 15 16:21:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,18 +4,20 @@ from .items import *
 # skus = unicode string
 def checkout(skus):
     tag_counts = Counter(skus)
+    print(tag_counts)
     items_list = [ItemA(), ItemB(), ItemC(), ItemD(), ItemE()]
     total_price = 0
 
-    for tag, count in tag_counts:
+    for tag, count in tag_counts.items():
         item = next((item for item in items_list if item.tag == tag), None)
 
         if item is None:
             return -1
         
-        total_price += item.calculate_cost(count)
+        total_price += item.calculate_cost(tag_counts.items())
         
     return total_price
+
 
 
 
