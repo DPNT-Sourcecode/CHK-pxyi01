946886cb15bec03a0b4ff666e1a017a67d9f2699
sbx_user1051 <sbx_user1051@169.254.52.149>
2024-07-15 15:51:15 +0000

    Mon Jul 15 15:51:15 UTC 2024

--- lib/solutions/CHK/Items.py
+++ lib/solutions/CHK/Items.py
@@ -15,6 +15,44 @@ class ItemA(Item):
         self.five_offer = 200
     
     def calculate_cost(self, items_count) -> int:
+        count = items_count[self.item]
+        cost = 0
+
+        cost += count // 5 * self.five_offer
+        count %= 5
+        cost += count // 3 * self.three_offer
+        count %= 3
+        cost += count * self.price
+        return cost
+    
+    def apply_discount(self, offer, count)
+    
+class ItemB(Item):
+
+    def __init__(self) -> None:
+        self.item = "B"
+        self.price = 30
+        self.two_offer_price = 45
+    
+    def calculate_cost(self, items_count) -> int:
+        count = items_count[self.item]
+        cost = 0
+
+        cost += count // 2 * self.two_offer
+        count %= 2
+        cost += count * self.price
+        return cost
+
+
+class ItemC(Item):
+
+    def __init__(self) -> None:
+        self.item = "A"
+        self.price = 50
+        self.three_offer = 130
+        self.five_offer = 200
+    
+    def calculate_cost(self, items_count) -> int:
         a_count = items_count[self.item]
         cost = 0
 
@@ -24,7 +62,44 @@ class ItemA(Item):
         a_count %= 3
         cost += a_count * self.price
         return cost
+
+class ItemD(Item):
+
+    def __init__(self) -> None:
+        self.item = "A"
+        self.price = 50
+        self.three_offer = 130
+        self.five_offer = 200
     
+    def calculate_cost(self, items_count) -> int:
+        a_count = items_count[self.item]
+        cost = 0
+
+        cost += a_count // 5 * self.five_offer
+        a_count %= 5
+        cost += a_count // 3 * self.three_offer
+        a_count %= 3
+        cost += a_count * self.price
+        return cost
+
+class ItemE(Item):
+
+    def __init__(self) -> None:
+        self.item = "A"
+        self.price = 50
+        self.three_offer = 130
+        self.five_offer = 200
+    
+    def calculate_cost(self, items_count) -> int:
+        a_count = items_count[self.item]
+        cost = 0
+
+        cost += a_count // 5 * self.five_offer
+        a_count %= 5
+        cost += a_count // 3 * self.three_offer
+        a_count %= 3
+        cost += a_count * self.price
+        return cost
 
 
 
