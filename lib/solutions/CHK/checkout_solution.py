d595c190f4a98338029d9049b8f1fcfc6fbf8c3e
sbx_user1051 <sbx_user1051@169.254.52.149>
2024-07-15 14:28:02 +0000

    Mon Jul 15 14:28:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,8 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    price_map = {"A" : 50, "B" : 30, "C" : 20, "D" : 15}
+    a_count = 0
+    b_count = 
     raise NotImplementedError()
+
