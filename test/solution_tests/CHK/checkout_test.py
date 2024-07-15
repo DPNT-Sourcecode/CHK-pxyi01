b2ef0e02cae81f3901452747896508ca563101cf
sbx_user1051 <sbx_user1051@169.254.52.149>
2024-07-15 16:33:15 +0000

    Mon Jul 15 16:33:15 UTC 2024

--- test/solution_tests/CHK/checkout_test.py
+++ test/solution_tests/CHK/checkout_test.py
@@ -1,4 +1,5 @@
 from lib.solutions.CHK.checkout_solution import checkout
+from lib.solutions.CHK.items import *
 
 class TestCheckout():
 
@@ -7,3 +8,9 @@ class TestCheckout():
         assert checkout("AAABCD") == 195
         assert checkout("AAAABBBCCD") == 310
         assert checkout("AABBG") == -1
+    
+    def test_ItemA_class(self):
+        item_counts = {"A" : 2}
+        item = ItemA()
+        cost = item.calculate_cost(item_counts)
+        assert cost == 100
