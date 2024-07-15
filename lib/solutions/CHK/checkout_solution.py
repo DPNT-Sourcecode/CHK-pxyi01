888351d10fec896109e88394c83ee8f97d94ae40
sbx_user1051 <sbx_user1051@169.254.52.149>
2024-07-15 14:31:02 +0000

    Mon Jul 15 14:31:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,11 @@
 def checkout(skus):
     price_map = {"A" : 50, "B" : 30, "C" : 20, "D" : 15}
     a_count = 0
-    b_count = 
-    raise NotImplementedError()
+    b_count = 0
+    total_price = 0
+
+    for item in skus:
+        total_price += 
+    return total_price
+
 
