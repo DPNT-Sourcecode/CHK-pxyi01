c2c9f08e6f8214c14825082a4946c68a22f0beb7
sbx_user1051 <sbx_user1051@169.254.10.173>
2024-07-22 16:33:06 +0000

    Mon Jul 22 16:33:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,22 +1,12 @@
 from collections import Counter
+from 
 #from lib.solutions.CHK.items import *
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    tag_counts = Counter(skus)
-    print(tag_counts)
-    # items_list = [ItemA(), ItemB(), ItemC(), ItemD(), ItemE()]
-    total_price = 0
+    item_counts = Counter(skus)
+    shopping = Shopping()
 
-    # for tag, count in tag_counts.items():
-    #     item = next((item for item in items_list if item.tag == tag), None)
-
-    #     if item is None:
-    #         return -1
-        
-    #     print(item)
-    #     total_price += item.calculate_cost(dict(tag_counts.items()))
-    #     print (total_price)
     return total_price
 
 
--- lib/solutions/CHK/items.py
+++ lib/solutions/CHK/items.py
@@ -9,6 +9,7 @@ class Shopping():
         total_cost = 0
 
         for item in self.items:
+            total_cost += Offer.get_discounted_cost(item)
             total_cost += item.cost * item.count
         
         return total_cost
@@ -28,7 +29,6 @@ class Offer():
             discounted_cost += item.count*item.cost
         
         return discounted_cost
-    
 
 class Item():
 
@@ -37,92 +37,3 @@ class Item():
         self.cost = cost
         self.count = count
             
-
-class ItemA(Item):
-
-    def __init__(self) -> None:
-        self.tag = "A"
-        self.price = 50
-    
-    def calculate_cost(self, items_count) -> int:
-        self.count = items_count[self.tag]
-        cost = 0
-
-        cost += self.apply_discount(130, 3)
-        cost += self.apply_discount(200, 5)
-        cost += self.count * self.price
-        return cost
-    
-    def apply_discount(self, discount, number_for_discount) -> int:
-        cost = self.count // number_for_discount * discount
-        self.count %= number_for_discount
-        return cost
-    
-class ItemB(Item):
-
-    def __init__(self) -> None:
-        self.tag_for_discount = "E"
-        self.tag = "B"
-        self.price = 30
-    
-    def calculate_cost(self, items_count) -> int:
-        self.count = items_count[self.tag]
-        e_count = items_count.get(self.tag_for_discount, 0)
-        cost = 0
-
-        self.count -= e_count // 2
-        cost += self.apply_discount(45, 2)
-        cost += self.count * self.price
-        return cost
-
-    def apply_discount(self, discount, number_for_discount) -> int:
-        cost = self.count // number_for_discount * discount
-        self.count %= number_for_discount
-        return cost
-
-class ItemC(Item):
-
-    def __init__(self) -> None:
-        self.tag = "C"
-        self.price = 20
-    
-    def calculate_cost(self, items_count) -> int:
-        self.count = items_count[self.tag]
-        cost = 0
-
-        cost += self.count * self.price
-        return cost
-
-class ItemD(Item):
-
-    def __init__(self) -> None:
-        self.tag = "D"
-        self.price = 15
-
-    def calculate_cost(self, items_count) -> int:
-        a_count = items_count[self.tag]
-        cost = 0
-
-        cost += a_count * self.price
-        return cost
-
-class ItemE(Item):
-
-    def __init__(self) -> None:
-        self.tag = "E"
-        self.price = 40
-    
-    def calculate_cost(self, items_count) -> int:
-        a_count = items_count[self.tag]
-        cost = 0
-
-        cost += a_count * self.price
-        return cost
-
-
-
-
-
-
-
-
