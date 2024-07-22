b21397451f413b1bfc2b5acbab0620c0f4729561
sbx_user1051 <sbx_user1051@169.254.10.173>
2024-07-22 16:39:06 +0000

    Mon Jul 22 16:39:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,13 +3,20 @@ from items import *
 #from lib.solutions.CHK.items import *
 # noinspection PyUnusedLocal
 # skus = unicode string
+item_costs = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+
 def checkout(skus):
     item_counts = Counter(skus)
     shopping = Shopping()
 
     for tag, count in item_counts.items():
-        item = Item(tag, count)
+        item_cost = item_costs.get(tag)
+        item = Item(tag, item_cost, count)
+        shopping.add_item(item)
+    
+    total_price = shopping.calculate_total_cost()
     return total_price
+
 
 
 
